cd54c1f31b08f235dc9b714b7e3fd6b68963a677
vivivaturi <vivek.ivaturi@gmail.com>
2021-06-01 13:51:24 -0400

    Worked on getting repo of interest

--- parsing.py
+++ parsing.py
@@ -9,11 +9,27 @@ import ast
 
 repo_name = "snorkel"
 
+home = os.path.expanduser("~")
+
+def find_dir(target, start):
+    for path, dirs, files in os.walk(start):
+        if target in dirs: 
+            return os.path.join(path, target)
+
+# Recursively determine where target repo is based on home directory
+repo_path = find_dir(repo_name, home)
+
+# Get path and change directory
+# Works if target directory is same depth as current directory 
 # git_path = os.getcwd().split(os.sep)[:-1]
 # git_path.append(repo_name)
 # repo_path = os.sep.join(git_path)
-# ### needs work
-#
+
+try:
+    os.chdir(repo_path)
+except OSError:
+    print("Error changing directory")
+
 # repo = Repo(repo_path)
 # assert not repo.bare
 # g = Git(repo_path)
