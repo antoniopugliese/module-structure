194668b4a6953c572bdc424b1f3f89a9bf90fa40
Jackson K Staniec <jks273@cornell.edu>
2021-06-07 14:44:11 -0700

    ImportEdge() is able to recognize imports from the same module

--- relationship.py
+++ relationship.py
@@ -17,7 +17,11 @@ import parsing
 import networkx as nx
 
 
+repo_name = "snorkel"
+
 # lists the name of every function in the ast
+
+
 class FuncLister(ast.NodeVisitor):
     def visit_FunctionDef(self, node):
         print(node.name)
@@ -38,14 +42,15 @@ class ImportEdge(ast.NodeVisitor):
         super().__init__()
         self.imported_mods = []
 
-    def visit_Import(self, node):
-        for alias in node.names:
-            self.imported_mods.append(alias.name)
-        self.generic_visit(node)
+    # def visit_Import(self, node):
+    #     for alias in node.names:
+    #         self.imported_mods.append(alias.name)
+    #     self.generic_visit(node)
 
     def visit_ImportFrom(self, node):
-        for alias in node.names:
-            self.imported_mods.append(alias.name)
+        mod = node.module
+        if mod == None or mod.startswith(repo_name):
+            self.imported_mods.append(node.module)
         self.generic_visit(node)
 
 
@@ -59,15 +64,16 @@ def import_relationship(tree):
         if type(node) is Node.FileNode:
             node_visitor.visit(node.get_ast())
 
-            print(f"The file '{node.name}' has the imports: ")
-            for mod_name in node_visitor.imported_mods:
-                print("\t" + mod_name)
-            print("\n")
-            node_visitor.imported_mods = []
+            if node_visitor.imported_mods != []:
+                print(f"The file '{node.name}' has the imports: ")
+                for mod_name in node_visitor.imported_mods:
+                    print("\t" + mod_name)
+                print("\n")
+                node_visitor.imported_mods = []
 
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
-repo_name = "snorkel"
+
 data_path = os.path.join(current_dir, "module_data")
 with open(os.path.join(data_path, repo_name), "rb") as file:
     ast_dict = pickle.load(file)
@@ -76,18 +82,6 @@ with open(os.path.join(data_path, repo_name), "rb") as file:
 first = list(ast_dict.keys())[1]
 import_relationship(ast_dict[first])
 
-
-# ast1 = ast.parse("import bruh")
-# ast2 = ast.parse("import bruh as b")
-# ast3 = ast.parse("from dude import bruh")
-# # print(ast.dump(ast.parse("import bruh")))
-# # print(ast.dump(ast.parse("import bruh as b")))
-# # print(ast.dump(ast.parse("from dude import bruh")))
-
-# node_visitor = ImportEdge()
-# node_visitor.visit(ast1)
-# print(node_visitor.imported_mods)
-# print(list(map(lambda n: n + ".py", node_visitor.imported_mods)))
 
 class AstGraph(nx.MultiGraph):
     """
