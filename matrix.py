ea934a41718c7a54fea8aaa768bfe4106cd73fda
vivivaturi <vivek.ivaturi@gmail.com>
2021-06-22 12:44:56 -0400

    New requirements, small fixes

---
+++ matrix.py
@@ -0,0 +1,11 @@
+"""
+This module will take in a networkx graph and construct an adjacency matrix from
+this graph. 
+"""
+
+import scipy as sp
+
+def graph_to_matrix(graph):
+	"""
+	Converts a networkx graph into a an adjacency matrix. 
+	"""
