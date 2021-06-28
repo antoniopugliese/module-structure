024803a9e18b4d6b43965357be54e38843fc9fd7
vivivaturi <vivek.ivaturi@gmail.com>
2021-06-28 16:59:28 -0400

    main.py implementation with databases

--- main.py
+++ main.py
@@ -7,6 +7,130 @@ import visual
 import metrics
 import matrix as mat
 from git import Repo, Git
+import redis
+
+
+def add_to_database(rs, key, value):
+    """
+    Adds a key and value to a database.
+
+    :param rs: a redis database
+    :type rs: redis.Redis
+
+    :param key: represents the key to be added for the database
+    :type key: str
+
+    :param value: represents the value to be added for the database
+    :type value: byte
+    """
+    rs.set(key, pickle.dumps(value))
+
+def get_from_database(rs, key):
+    """
+    Gets the value corresponding to a key.
+
+    :param rs: a redis database
+    :type rs: redis.Redis
+
+    :param key: represents the key for the database
+    :type key: str 
+
+    :return: the value associated with the key
+    :rtype: Multiple types
+    """
+    try: 
+        return pickle.loads(rs.get(key))
+    except TypeError:
+        return None
+
+def main_db():
+    rs = redis.Redis(db=0)
+    home = os.path.expanduser("~")
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+
+    with open(os.path.join(current_dir, "config.json"), "r") as f:
+        config = json.load(f)
+
+    repo_name = config["repo_name"]
+
+    print("Finding path to target repo...", end="", flush=True)
+
+    repo_path = get_from_database(rs, "repo_path")
+
+    if repo_path is not None:
+        print("Found path.")
+    else: 
+        print("Scanning start directory...", end="", flush=True)
+        repo_path = parsing.find_dir(home, repo_name)
+        add_to_database(rs, "repo_path", repo_path)
+    
+    try:
+        os.chdir(repo_path)
+    except OSError:
+        print("Error changing directory") 
+    
+    # create Repo object and extract list of commits
+    repo = Repo(repo_path)
+    assert not repo.bare
+    g = Git(repo_path)
+    g.checkout(config["branch"])
+    # limited to 10 for testing
+    commits = list(repo.iter_commits(
+        config["branch"], max_count=config["max_count"]))
+    
+    print("Checking if file is in database...", end="", flush=True)
+
+    ast_dict = get_from_database(rs, "ast_dict")
+
+    if ast_dict is not None: 
+        ast_dict = parsing.update_ast_dict(
+            ast_dict, commits, repo_path, repo_name, g)
+    else: 
+        print("Not Found.")
+        ast_dict = parsing.create_ast_dict(commits, repo_path, repo_name, g)
+        add_to_database(rs, "ast_dict", ast_dict)
+
+    #commit_list = list(ast_dict.keys())
+    # graph_dict = {}
+
+    # for commit in commit_list:
+    #     new_graph = rel.create_all_relationships(ast_dict[commit])
+    #     matrix = mat.graph_to_matrix(new_graph)
+    #     eigenvalues = mat.calculate_eig(new_graph)
+    #     graph_dict.update({commit: (new_graph, matrix, eigenvalues)})
+
+    # print("Done.")
+
+    # first = commit_list[0]
+    # tup = graph_dict.get(first)
+    # visual.display(tup[0])
+
+    # commit_dict = dict(map(lambda key:
+    #                        (key, rel.create_all_relationships(ast_dict[key])), ast_dict))
+
+    # first = commit_list[0]
+    # tup = graph_dict.get(first)
+    # visual.display(tup[0])
+
+    print("Checking if relationships have been formed...", end="", flush=True)
+
+    commit_dict = get_from_database(rs, "commit_dict")
+
+    if commit_dict is not None:
+        print("Found a file.")
+    else:
+        print("Not Found.")
+        commit_dict = dict(map(lambda key:
+                            (key, rel.create_all_relationships(ast_dict[key])), ast_dict)) 
+        print("Storing relationships...", end="", flush=True)
+        add_to_database(rs, "commit_dict", commit_dict)
+        print("Done.")
+
+    print("Done.\n")
+
+    visual.display(commits, commit_dict)
+
+
 
 
 def main():
@@ -121,4 +245,5 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    # main()
+    main_db()
